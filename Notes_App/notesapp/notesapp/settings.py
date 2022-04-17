0445456bfd07a3a9ca7bbf0c4395af6abe70a7e6
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-04-17 14:42:03 +0300

    docker integration

--- Notes_App/notesapp/notesapp/settings.py
+++ Notes_App/notesapp/notesapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c!%!%kquj88*fhwl899cc#7a!96^(5z&$*js6w61--+k^x10mp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
